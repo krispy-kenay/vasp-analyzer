9688bcb2648fd81f6a046fc3e792620a56a2edbc
krispy-kenay <yannick.gachnang@gmail.com>
2023-04-13 16:52:46 +0200

    update: change to different vasp handler

--- src/vasp-analyzer/__init__.py
+++ src/vasp-analyzer/__init__.py
@@ -3,7 +3,8 @@
 from __future__ import annotations
 import os
 
-from data_handler import VASP
+#from data_handler import VASP
+from vasp import VASP
 from plot_handler import vPlot, hPlot
 
 __author__ = "Yannick G."
