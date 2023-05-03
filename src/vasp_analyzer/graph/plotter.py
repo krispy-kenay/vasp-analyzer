12e5d1ca0b2c3e6706119deabf1c64ed7c31a16f
krispy-kenay <yannick.gachnang@gmail.com>
2023-05-03 19:28:56 +0200

    update: add interpolate surface function

--- src/vasp_analyzer/graph/plotter.py
+++ src/vasp_analyzer/graph/plotter.py
@@ -1,6 +1,6 @@
-import numpy as np
 import plotly
 import plotly.graph_objects as go
+import numpy as np
 from scipy import interpolate
 
 '''
@@ -16,11 +16,20 @@ VASP Plotter
 class vPlot:
     def __init__(self, width=None, height=None):
         """
-        Plotly plotter object to facilitate plotting 3D interactive graphs. Includes helper functions to draw hexagonal brillouin zone, create surface and scatter plots and more. Relies on the offline module to prevent sending data to external cloud servers
+        Plotly plotter object to facilitate plotting interactive graphs. Includes helper functions to draw hexagonal brillouin zone, create surface and scatter plots and more. Relies on the offline module to prevent sending data to external cloud servers
         """
         plotly.offline.init_notebook_mode()
         self.fig = go.Figure()
 
         self.w = width
         self.h = height
-        self.layout = None
+    
+    @staticmethod
+    def interpolate_surface(xi, yi, wi, density:int=500, method:str='linear'):
+
+        xxi = np.linspace(min(xi), max(xi), density)
+        yyi = np.linspace(min(yi), max(yi), density)
+        xx, yy = np.meshgrid(xxi, yyi)
+
+        zz = interpolate.griddata((xi, yi), wi, (xx, yy), method=method)
+        return xxi, yyi, zz
