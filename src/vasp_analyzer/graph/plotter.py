79c907976c7f233e55b8fd49a29107c642175f32
krispy-kenay <yannick.gachnang@gmail.com>
2023-05-12 17:13:42 +0200

    bugfix: rename functions properly

--- src/vasp_analyzer/graph/plotter.py
+++ src/vasp_analyzer/graph/plotter.py
@@ -335,8 +335,8 @@ class vplotter:
     @staticmethod
     def get_color_gradient(c1, c2, n):
         assert n > 1
-        c1_rgb = np.array(vPlot.hex_to_RGB(c1))/255
-        c2_rgb = np.array(vPlot.hex_to_RGB(c2))/255
+        c1_rgb = np.array(vplotter.hex_to_RGB(c1))/255
+        c2_rgb = np.array(vplotter.hex_to_RGB(c2))/255
         mix_pcts = [x/(n-1) for x in range(n)]
         rgb_colors = [((1-mix)*c1_rgb + (mix*c2_rgb)) for mix in mix_pcts]
         return ["#" + "".join([format(int(round(val*255)), "02x") for val in item]) for item in rgb_colors]
